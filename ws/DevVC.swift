83400077b3a88b27c40d6c68c9ad8edf809e2bb6
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-01-31 00:48:31 +0300

    done

--- ws/DevVC.swift
+++ ws/DevVC.swift
@@ -1,121 +1,11 @@
 #if DEBUG
 import UIKit
 import os
-//
-//struct Event: Codable {
-//  let id: String
-//  let name: String
-//}
-//
-//struct Event2: Codable {
-//  let id: Int
-//}
-//
-//struct CallRequest: Encodable {
-//  let id: String
-//}
-//
-//struct Profile: Decodable {
-//  let name: String
-//}
-//
-//struct CallResponse: Decodable {
-//  let id: String
-//  let iceServers: [URL]
-//  let profile: Profile
-//  let date: Date?
-//  let point: CGPoint?
-//}
-
-//let req = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: timeout)
-//URLSession.shared.dataTask(with: req) { <#Data?#>, <#URLResponse?#>, <#Error?#> in
-//  <#code#>
-//}
- 
-//fileCache.get(voicemail.url, for: voicemail.key) { [weak self] result in
-//  switch result {
-//  case let .success(data): self?.play(audio: data)
-//  case let .failure(error): print("error", error)
-//  }
-//}
-//
-//final class FileCache {
-//  private let lockQueue = DispatchQueue(label: "filecache.lock.queue")
-//  private var callbacks = [String: [(Result<Data, Swift.Error>) -> Void]]()
-//  private let root: URL
-//
-//  enum Error: Swift.Error {
-//    case network
-//  }
-//  // TODO timeout
-//  func get(_ url: URL, for key: String, callback: @escaping (Result<Data, Swift.Error>) -> Void) {
-//    let keyURL = root.appendingPathComponent(key)
-//
-//    lockQueue.async { [weak self] in
-//      guard let self = self else { return }
-//
-//      // data already cached
-//      if let data = FileManager.default.contents(atPath: keyURL.path) {
-//        callback(.success(data))
-//      } else {
-//        if var callbacks = self.callbacks[key] {
-//          callbacks.append(callback)
-//          return
-//        }
-//
-//        self.callbacks[key] = [callback]
-//
-//        // or just use URLCache
-//        let task = URLSession.shared.dataTask(with: url) { data, resp, error in
-//          self.lockQueue.async { [weak self] in
-//            guard let self = self else { return }
-//            guard let callbacks = self.callbacks.removeValue(forKey: key) else { return }
-//
-//            if let error = error {
-//              callbacks.forEach { $0(.failure(error)) }
-//              return
-//            }
-//
-//            guard let resp = resp as? HTTPURLResponse else {
-//              callbacks.forEach { $0(.failure(Error.network)) }
-//              return
-//            }
-//
-//            guard resp.statusCode == 200 else {
-//              callbacks.forEach { $0(.failure(Error.network)) }
-//              return
-//            }
-//
-//            guard let data = data else {
-//              callbacks.forEach { $0(.failure(Error.network)) }
-//              return
-//            }
-//
-//            callbacks.forEach { $0(.success(data)) }
-//
-//            do {
-//              try data.write(to: keyURL) // TODO .atomic
-//            } catch {
-//              // TODO
-//              print("error wtiring \(data) to \(keyURL) for url=\(url) and key=\(key)", error)
-//            }
-//          }
-//        }
-//
-//        // can also return progress
-//        task.resume()
-//      }
-//    }
-//  }
-//
-//  init(root: URL) {
-//    self.root = root
-//  }
-//}
 
 class DevVC: UIViewController {
   var socket: Socket!
-  let lockQueue = DispatchQueue(label: "name.lock.queue", attributes: .concurrent)
+  let lockQueue = DispatchQueue(label: "name.lock.queue")
+  var counter = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -131,54 +21,233 @@ class DevVC: UIViewController {
     run()
   }
   
-  // ~~I need to learn more about timers, how to schedule them from background using runloop, when it can fail etc~~ just use dispatch queues .asyncAfter
-  // I need to learn more about dispatch queues, self capture etc
-  // check that github gist about gcd
-  // learn about thread sanitizer https://twitter.com/twannl/status/1192781427978493952?s=20
-  // watch that wwdc session on gcd
-  // check out firebase crashlytics
-  // https://developer.apple.com/videos/play/wwdc2018/414/
-  // https://developer.apple.com/videos/play/wwdc2021/10203
-  // https://developer.apple.com/videos/play/wwdc2020/10078
-  // https://developer.apple.com/videos/play/wwdc2018/412
-  // https://developer.apple.com/videos/play/wwdc2018/416
-  // https://developer.apple.com/videos/play/wwdc2018/401
-  // https://developer.apple.com/videos/play/wwdc2017/406
-  // try lock with thread sanitizer
   private func run() {
     print("\n[RUN]")
     
-    let started = Date()
-    let lock = NSRecursiveLock()
-    
-    (1...6_000_000).forEach { i in
-      lock.lock()
-      _ = i + 1
-      lock.unlock()
-    }
-    
-    let ended = Date()
-    print("done in", ended.timeIntervalSince1970 - started.timeIntervalSince1970)
-    
+//    let queue = OperationQueue()
+//    queue.underlyingQueue = lockQueue
+//
+//    let pairs = [(2, 3), (5, 3), (1, 7), (12, 34), (99, 99)]
+//
+//    pairs.forEach { pair in
+//      let op = AsyncSumOperation(lhs: pair.0, rhs: pair.1)
+//
+//      op.completionBlock = {
+//        print("\(pair.0) + \(pair.1) = \(op.result!)")
+//      }
+//
+//      queue.addOperation(op)
+//    }
+
 //    lockQueue.async {
-//      print("here 1", Thread.current)
-//
-//      self.lockQueue.async {
-//        self.lockQueue.async {
-//          print("here 5", Thread.current)
-//        }
-//
-//        print("here 3", Thread.current)
+//      print("1")
+//      for _ in 1...10000 {
+//        self.counter += 1
 //      }
+//      print("2")
+//    }
 //
-//      print("here 2", Thread.current)
-//
-//      self.lockQueue.async {
-//        print("here 4", Thread.current)
+//    DispatchQueue.main.async {
+//      print("3")
+//      for _ in 1...10000 {
+//        self.counter += 1
 //      }
+//      print("4")
 //    }
   }
 }
+
+final class AsyncSumOperation: AsyncOperation {
+  let rhs: Int
+  let lhs: Int
+  var result: Int?
+  
+  init(lhs: Int, rhs: Int) {
+    self.rhs = rhs
+    self.lhs = lhs
+    super.init()
+  }
+  
+  override func main() {
+    Thread.sleep(forTimeInterval: 2)
+    self.result = self.lhs + self.rhs
+    self.state = .finished
+  }
+}
+
+typealias ImageOperationCompletion = (Data?, URLResponse?, Error?) -> Void
+
+final class NetworkImageOperation: AsyncOperation {
+  var image: UIImage?
+  
+  private let url: URL
+  private let completion: ImageOperationCompletion?
+  
+  init(url: URL, completion: ImageOperationCompletion? = nil) {
+    self.url = url
+    self.completion = completion
+    super.init()
+  }
+  
+  convenience init?(string: String, completion: ImageOperationCompletion? = nil) {
+    guard let url = URL(string: string) else { return nil }
+    self.init(url: url, completion: completion)
+  }
+  
+  override func main() {
+    URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+      guard let self = self else { return }
+      defer { self.state = .finished }
+      
+      if let completion = self.completion {
+        completion(data, response, error)
+        return
+      }
+      
+      guard error == nil, let data = data else { return }
+      self.image = UIImage(data: data)
+    }.resume()
+  }
+}
+
+func callbacks() {
+  listUsers { result in
+    guard case let .success(users) = result else { return }
+    
+    loadAvatars(for: users) { result in
+      guard case let .success(usersAndAvatars) = result else { return }
+      DispatchQueue.main.async { print(usersAndAvatars) }
+    }
+  }
+  
+  // listUsers().then { users in loadAvatars(for: users) }
+}
+
+struct User {
+  let name: String
+}
+
+func listUsers(callback: @escaping (Result<[User], Error>) -> Void) {
+  DispatchQueue.main.async {
+    let users = [User(name: "1"), User(name: "2")]
+    callback(.success(users))
+  }
+}
+
+func loadAvatars(for users: [User], callback: @escaping (Result<[(User, UIImage?)], Error>) -> Void) {
+  DispatchQueue.main.async {
+    let usersAndAvatars = users.map { user in (user, UIImage(data: Data())) }
+    callback(.success(usersAndAvatars))
+  }
+}
+
+//func duration(_ block: () -> Void) -> TimeInterval {
+//  let start = Date()
+//  block()
+//  return Date().timeIntervalSince(start)
+//}
+
+class AsyncOperation: Operation {
+  enum State: String {
+    case ready, executing, finished
+    
+    fileprivate var keyPath: String {
+      "is\(rawValue.capitalized)"
+    }
+  }
+  
+  var state = State.ready {
+    willSet {
+      willChangeValue(forKey: newValue.keyPath)
+      willChangeValue(forKey: state.keyPath)
+    }
+    
+    didSet {
+      didChangeValue(forKey: oldValue.keyPath)
+      didChangeValue(forKey: state.keyPath)
+    }
+  }
+  
+  override var isReady: Bool {
+    super.isReady && state == .ready
+  }
+  
+  override var isExecuting: Bool {
+    state == .executing
+  }
+  
+  override var isFinished: Bool {
+    state == .finished
+  }
+  
+  override var isAsynchronous: Bool {
+    true
+  }
+  
+  override func start() {
+    if isCancelled {
+      state = .finished
+      return
+    }
+    
+    main()
+    state = .executing
+  }
+}
+
+//
+//final class TiltShiftOperation: Operation {
+//  private static let context = CIContext()
+//  var outputImage: UIImage?
+//
+//  private let inputImage: UIImage
+//
+//  init(image: UIImage) {
+//    inputImage = image
+//    super.init()
+//  }
+//
+//  override func main() {
+//    print("running main")
+//    sleep(2)
+//    outputImage = inputImage
+//  }
+//}
+
+extension DispatchQueue {
+  static func ensureMain(_ block: @escaping () -> Void) {
+    if Thread.isMainThread {
+      block()
+    } else {
+      main.async(execute: block)
+    }
+  }
+}
+
+//Promise { resolve, reject in
+//  resolve(123)
+//}.then { i in
+//  print(i)
+//}
+
+//typealias Resolve<T> = (T) -> Void
+//typealias Reject<Error> = (Error) -> Void
+//
+//final class Promise<T> {
+//  private let block: (Resolve<T>, Reject<Error>) -> Void
+//  private var next: Promise?
+//
+//  init(_ block: @escaping (Resolve<T>, Reject<Error>) -> Void) {
+//    let resolve: Resolve<T> = { result in (result) }
+//    let reject: Reject<Error> = { error in }
+//  }
+//
+//  func then<T>(_ block: @escaping (Resolve<T>, Reject<Error>)) -> Promise<T> {
+//    let promise = Promise(block)
+//    next = promise
+//    return promise
+//  }
+//}
 
 func retry(attempts: UInt = 2, block: @escaping (@escaping (Bool) -> Void) -> Void) {
   guard attempts > 0 else { return }
